bb917cdc3c287b4677ce3ee583ac304c783c3f1a
Hoai Duc <hoaiduc2000@gmail.com>
2016-06-23 08:33:41 +0700

    Update recycler view

--- app/src/main/java/com/example/nguyenhoaiduc/layout_practice/RecyclerViewActivity.java
+++ app/src/main/java/com/example/nguyenhoaiduc/layout_practice/RecyclerViewActivity.java
@@ -3,16 +3,22 @@ package com.example.nguyenhoaiduc.layout_practice;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import adapter.ArticleAdapter;
+import model.Article;
 
 /**
  * Created by nguyen.hoai.duc on 6/22/2016.
  */
 public class RecyclerViewActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
+    private ArticleAdapter mArticleAdapter;
+    private ArrayList<Article> list;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +28,15 @@ public class RecyclerViewActivity extends AppCompatActivity {
     public void initData(){
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
         mRecyclerView.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(this);
+        list = new ArrayList();
+        for(int i=0;i<200;i++)
+            list.add(new Article("fsdf","fsgs"));
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
         mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mArticleAdapter = new ArticleAdapter(list);
+        mRecyclerView.setAdapter(mArticleAdapter);
+        mArticleAdapter.notifyDataSetChanged();
+
     }
 }
