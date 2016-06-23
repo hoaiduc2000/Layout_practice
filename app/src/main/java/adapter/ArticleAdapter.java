82fde18395c6dd79c6882c28783040ea065b15ac
Hoai Duc <hoaiduc2000@gmail.com>
2016-06-23 16:06:04 +0700

    Fix convention code

--- app/src/main/java/adapter/ArticleAdapter.java
+++ app/src/main/java/adapter/ArticleAdapter.java
@@ -66,7 +66,6 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.MyViewHo
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView mTextView;
-
         public MyViewHolder(View view) {
             super(view);
             mTextView = (TextView) view.findViewById(R.id.text_view_article);
