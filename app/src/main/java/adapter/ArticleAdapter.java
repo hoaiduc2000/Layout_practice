74837e75a5e8b54a010a3c82a18be13084dfe929
Hoai Duc <hoaiduc2000@gmail.com>
2016-06-23 10:34:03 +0700

    new update

--- app/src/main/java/adapter/ArticleAdapter.java
+++ app/src/main/java/adapter/ArticleAdapter.java
@@ -1,5 +1,6 @@
 package adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,15 +17,21 @@ import model.Article;
  * Created by nguyen.hoai.duc on 6/22/2016.
  */
 public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.MyViewHolder> {
-    private ArrayList<Article> list;
-    public class MyViewHolder extends RecyclerView.ViewHolder{
-        public TextView content;
-        public MyViewHolder(View view){
-            super(view);
-        }
+
+    public OnClickListener mOnClickListener;
+
+    public interface OnClickListener{
+        void OnClickListener(String content);
     }
-    public ArticleAdapter(ArrayList<Article> list){
-        this.list = list;
+
+    private ArrayList<Article> mArticleArrayList;
+    private Context mContext;
+
+
+    public ArticleAdapter(Context context, ArrayList<Article> arrayList) {
+        this.mContext = context;
+        this.mArticleArrayList = arrayList;
+        this.mOnClickListener = null;
     }
 
     @Override
@@ -35,11 +42,34 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.MyViewHo
 
     @Override
     public void onBindViewHolder(ArticleAdapter.MyViewHolder holder, int position) {
+        final Article article = mArticleArrayList.get(position);
+        final String content = holder.content.getText().toString();
 
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mOnClickListener != null) {
+                    mOnClickListener.OnClickListener(content);
+                }
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return list.size();
+        return mArticleArrayList.size();
+    }
+
+    public void setOnclickListener(OnClickListener onclickListener) {
+        this.mOnClickListener = onclickListener;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        public TextView content;
+
+        public MyViewHolder(View view){
+            super(view);
+            content = (TextView) view.findViewById(R.id.tv_article);
+        }
     }
 }
--- app/src/main/java/com/example/nguyenhoaiduc/layout_practice/ArticleActivity.java
+++ app/src/main/java/com/example/nguyenhoaiduc/layout_practice/ArticleActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 
@@ -15,18 +16,20 @@ import model.Article;
 /**
  * Created by nguyen.hoai.duc on 6/22/2016.
  */
-public class RecyclerViewActivity extends AppCompatActivity {
+public class ArticleActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private ArticleAdapter mArticleAdapter;
     private ArrayList<Article> list;
+    private ImageView mImageView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recyclerview);
+        setContentView(R.layout.activity_article);
         initData();
     }
     public void initData(){
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        mImageView = (ImageView) findViewById(R.id.iv_image);
         mRecyclerView.setHasFixedSize(true);
         list = new ArrayList();
         for(int i=0;i<200;i++)
@@ -34,7 +37,7 @@ public class RecyclerViewActivity extends AppCompatActivity {
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
-        mArticleAdapter = new ArticleAdapter(list);
+        mArticleAdapter = new ArticleAdapter(this,list);
         mRecyclerView.setAdapter(mArticleAdapter);
         mArticleAdapter.notifyDataSetChanged();
 
